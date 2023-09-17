102d6bcbb20d59a101ad372a217de4c2e4445e18
PranoSA <pcadler@gmail.com>
2023-09-16 20:39:44 -0700

    Typescript on QUeries

--- Chat_Server/src/routes/message_routes.ts
+++ Chat_Server/src/routes/message_routes.ts
@@ -1,4 +1,6 @@
+import express, { Request, Response, NextFunction } from 'express';
 import MessageModel from "../message_models/message_models"
+import { types as CassandraTypes } from 'cassandra-driver';
 
 
 class MessageRoutes  {
@@ -8,6 +10,23 @@ class MessageRoutes  {
         this.models = model 
     }
 
+
+
+    async SearchMessage (req:Request, res:Response, next:NextFunction){
+        const chatid: number = parseInt(req.params.chatid)
+
+        //const cursor:number = parseInt(req.params.start) || Date.now()
+        
+        const cursor:number = parseInt(req.query.start as string )
+
+        const limit: number = parseInt(req.query.limit as string)
+    
+        const terms : string[] = req.query.terms as string[]
+        
+
+    }
+
 }
+
 
 export default MessageRoutes 
